bd6a679c17a190a90c465d7a32f8e71df8266b53
E.Aslankarayigit <117314447+aslankarayigitemirhan@users.noreply.github.com>
2025-08-12 16:12:49 +0300

    Delete build directory

--- build/WSL_GCC-Release/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/aslankarayigitemirhan/internship/build/WSL_GCC-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/lib/qt5/bin/qmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/usr" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/x86_64-linux-gnu-gcc" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/x86_64-linux-gnu-g++" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
